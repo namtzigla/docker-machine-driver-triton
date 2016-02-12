45844cd63784ec3c27f61d800a0c6469bde1cf3f
Florin Stan <tom@tcnp3.com>
2016-02-12 15:17:23 -0700

    cleanup

--- src/triton/cryptoops.go
+++ src/triton/cryptoops.go
@@ -2,40 +2,9 @@ package triton
 
 import (
 	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"github.com/yosida95/golang-sshkey"
-	"golang.org/x/crypto/ssh"
 	"io/ioutil"
-	"os"
 )
-
-func MakeSSHKeyPair(pubKeyPath, privateKeyPath string) error {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		return err
-	}
-
-	// generate and write private key as PEM
-	privateKeyFile, err := os.Create(privateKeyPath)
-	defer privateKeyFile.Close()
-	if err != nil {
-		return err
-	}
-	privateKeyPEM := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}
-	if err := pem.Encode(privateKeyFile, privateKeyPEM); err != nil {
-		return err
-	}
-
-	// generate and write public key
-	pub, err := ssh.NewPublicKey(&privateKey.PublicKey)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(pubKeyPath, ssh.MarshalAuthorizedKey(pub), 0655)
-}
 
 func GetSshKey(path string) (sshkey.PublicKey, error) {
 	// try to load ssh public key from path
@@ -50,4 +19,13 @@ func GetSshKey(path string) (sshkey.PublicKey, error) {
 
 func GetSshKeyFingerprint(key sshkey.PublicKey) (string, error) {
 	return sshkey.PrettyFingerprint(key, crypto.MD5)
+}
+
+func GetSshKeyId(path string) (string, error) {
+	key, err := GetSshKey(path)
+	if err != nil {
+		return "", err
+	}
+
+	return GetSshKeyFingerprint(key)
 }
