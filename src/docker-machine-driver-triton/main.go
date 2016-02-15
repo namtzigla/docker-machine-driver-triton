9e2d06e38e28095fd18efe8ecb33a8dbf8a54715
Florin Stan <tom@tcnp3.com>
2016-02-15 10:57:07 -0700

    print out version info when is called directly

--- src/docker-machine-driver-triton/main.go
+++ src/docker-machine-driver-triton/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
+	"github.com/docker/machine/libmachine/drivers/plugin/localbinary"
 
+	"fmt"
+	"os"
 	"triton"
 )
 
 func main() {
+	if os.Getenv(localbinary.PluginEnvKey) != localbinary.PluginEnvVal {
+		fmt.Printf("VERSION: %s, COMMIT: %s\n", Version, GitCommit)
+	}
 	plugin.RegisterDriver(new(triton.Driver))
 }
